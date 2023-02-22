b1586dd1216ecccd94a478e51cdfda6083275ca3
BuckLuckDuck <nikitos80011@gmail.com>
2023-02-22 15:49:09 +0700

    Initial commit

---
+++ src/main/java/com/example/iotapp/IoTAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.iotapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoTAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iotapp/IoTAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iotapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
